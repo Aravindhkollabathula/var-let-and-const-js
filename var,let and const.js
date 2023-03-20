0b31e32692c10ee1395d0789c95f5edf9442ad49
Aravindhkollabathula <125636628+Aravindhkollabathula@users.noreply.github.com>
2023-03-20 11:54:50 +0530

    Add files via upload

---
+++ var,let and const.js
@@ -0,0 +1,46 @@
+// var type
+
+// var a = 10
+// console.log(a)
+
+// var b;
+// console.log(b)
+// b = 50;
+// console.log(b)
+// b = 40
+// console.log(b) 
+
+// console.log(a)
+// var a = 10;
+
+// console.log(b)
+// let b;
+
+// let b
+// console.log(b)
+
+// console.log(a)
+// var a;
+// console.log(a)
+// var a = 10
+// console.log(a)
+
+// let type
+
+// console.log(a)
+// let a
+// console.log(a)
+// a = 10
+// console.log(a)
+
+// let a =60;
+// console.log(a)
+
+// const type
+
+
+const laptopprice = 66000
+console.log(laptopprice)
+
+laptopprice = 55000
+console.log(laptopprice)
